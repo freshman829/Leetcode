05db86f543b28c112c207a448e2545499189417c
Sirius <133846033+sirius829@users.noreply.github.com>
2024-01-04 19:31:12 +0900

    Time: 139 ms (38.46%) | Memory: 58.5 MB (25.64%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+    const counts = {}; // Store counts for each unique number
+    for (const num of nums) {
+    counts[num] = (counts[num] || 0) + 1;
+    }
+
+    // Check for impossibility
+    for (const count of Object.values(counts)) {
+    if (count === 1 || getOpCnt(count) === -1 || getOpCnt(count) === Infinity) return -1;
+    }
+
+    const mostFreqNum = Object.keys(counts).reduce((maxNum, num) => {
+    return counts[num] > counts[maxNum] ? num : maxNum;
+    }, 0);
+
+    let totalOpCnt = 0;
+    for (const num in counts) {
+    if (num !== mostFreqNum) {
+      const count = counts[num];
+      const minOps = getOpCnt(count); // Or use a lookup table
+      if (minOps === Infinity || minOps === -1) return -1;
+      totalOpCnt += minOps;
+    }
+    }
+
+    return totalOpCnt;
+};
+
+var getOpCnt = (len) => {
+    if (len % 3 === 0) return len / 3;
+    if (len === 2) return 1;
+    for (let i = Math.floor(len / 3); i >= 0; i--) {
+        let free = len - i * 3;
+        if (free % 2 == 0) {
+            return i + free / 2;
+        }
+    }
+    return -1;
+}
