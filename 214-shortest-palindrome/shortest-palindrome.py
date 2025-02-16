18ee1332d1828cd6c5297cca7f73d7ad14aa1971
Sirius <133846033+sirius829@users.noreply.github.com>
2025-02-16 22:45:57 +0100

    Time: 118 ms (31.96%) | Memory: 14.5 MB (53.09%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -4,23 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        base = 131
-        mod = 10**9 + 7
-        n = len(s)
-        prefix = suffix = 0
-        mul = 1
-        idx = 0
-        for i, c in enumerate(s):
-            prefix = (prefix * base + (ord(c) - ord('a') + 1)) % mod
-            suffix = (suffix + (ord(c) - ord('a') + 1) * mul) % mod
-            mul = (mul * base) % mod
-            if prefix == suffix:
-                idx = i + 1
-        return s if idx == n else s[idx:][::-1] + s
-        # t = s[::-1]
+        t = s[::-1]
 
-        # for i in range(len(t)):
-        #     if s.startswith(t[i:]):
-        #         return t[:i] + s
+        for i in range(len(t)):
+            if s.startswith(t[i:]):
+                return t[:i] + s
 
-        # return t + s
+        return t + s
