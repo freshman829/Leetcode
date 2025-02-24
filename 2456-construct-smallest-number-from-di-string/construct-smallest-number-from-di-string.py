f4c8a4cca34eaddc8ecc23fbba27a4592eba0162
Sirius <133846033+sirius829@users.noreply.github.com>
2025-02-24 13:34:17 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.87%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,17 +1,26 @@
-class Solution(object):
-    def smallestNumber(self, pattern):
-        """
-        :type pattern: str
-        :rtype: str
-        """
-        result = []
-        for i in range(len(pattern) + 1):
-            if not (i == len(pattern) or pattern[i] == 'I'):
-                continue
-            for x in reversed(range(len(result) + 1, (i + 1) + 1)):
-                result.append(x)
-        return "".join(map(str, result))
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        def dfs(u):
+            nonlocal ans
+            if ans:
+                return
+            if u == len(pattern) + 1:
+                ans = ''.join(t)
+                return
+            for i in range(1, 10):
+                if not vis[i]:
+                    if u and pattern[u - 1] == 'I' and int(t[-1]) >= i:
+                        continue
+                    if u and pattern[u - 1] == 'D' and int(t[-1]) <= i:
+                        continue
+                    vis[i] = True
+                    t.append(str(i))
+                    dfs(u + 1)
+                    vis[i] = False
+                    t.pop()
 
-
-            
-            
+        vis = [False] * 10
+        t = []
+        ans = None
+        dfs(0)
+        return ans
